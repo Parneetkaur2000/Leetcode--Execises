58fae7e141b0ce93cd6b1e49f31508461eb958bd
Parneet Kaur <62416642+Parneetkaur2000@users.noreply.github.com>
2020-08-20 16:55:44 +0530

    Find and Replace pattern

---
+++ Find and replace pattern.py
@@ -0,0 +1,32 @@
+You have a list of words and a pattern, and you want to know which words in words matches the pattern.
+
+A word matches the pattern if there exists a permutation of letters p so that after replacing every letter x in the pattern with p(x), we get the desired word.
+
+(Recall that a permutation of letters is a bijection from letters to letters: every letter maps to another letter, and no two letters map to the same letter.)
+
+Return a list of the words in words that match the given pattern. 
+
+You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: words = ["abc","deq","mee","aqq","dkd","ccc"], pattern = "abb"
+Output: ["mee","aqq"]
+Explanation: "mee" matches the pattern because there is a permutation {a -> m, b -> e, ...}. 
+"ccc" does not match the pattern because {a -> c, b -> c, ...} is not a permutation,
+since a and b map to the same letter.
+
+*******************************************************
+
+class Solution(object):
+    def findAndReplacePattern(self, words, pattern):
+        """
+        :type words: List[str]
+        :type pattern: str
+        :rtype: List[str]
+        """
+        
+        return [i for i in words if len(set(zip(pattern,i)))==len(set(pattern))==len(set(i))]
+        
